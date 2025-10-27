f15c9646085631f3c648fffb2f8b538822516179
alondra-gs <alondra_gs@berkeley.edu>
2025-10-27 10:02:30 -0700

    Delete map-box directory

--- map-box/README.md
+++
@@ -1,6 +0,0 @@
-# MAP BOX
-
-A Pen created on CodePen.
-
-Original URL: [https://codepen.io/alondra-gs-the-scripter/pen/gbPKyVG](https://codepen.io/alondra-gs-the-scripter/pen/gbPKyVG).
-
--- map-box/dist/script.js
+++
@@ -1,7 +0,0 @@
-mapboxgl.accessToken = 'pk.eyJ1IjoiYWxvbmRyYS1ncyIsImEiOiJjbWg5Y3BwYTMwNTczMnJwb2ZvcXRvZnppIn0.kpC54UdVbonYX2QJmIv3Fw';
-const map = new mapboxgl.Map({
-  container: 'map', // container ID
-  style: 'mapbox://styles/alondra-gs/cmh9d50gw00q001smgprd25qj', //Your Style URL goes here
-  center: [-122.27, 37.87], // starting position [lng, lat]. Note that lat must be set between -90 and 90
-  zoom: 9 // starting zoom
-    });
