b9d17134724852e6f4bbafcd081258aa15681ad2
alondra-gs <alondra_gs@berkeley.edu>
2025-11-07 19:48:49 -0800

    Update script.js

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ const map = new mapboxgl.Map({
         type: 'circle',
         source: 'points-data',
         paint: {
-              'circle-color': '#FF69B4',
+              'circle-color': '#4264FB',
               'circle-radius': 6,
               'circle-stroke-width': 2,
               'circle-stroke-color': '#ffffff'
